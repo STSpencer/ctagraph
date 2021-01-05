bf5a5816379b5adfadb7dbf1c16021181b40e3f6
Samuel Spencer <samuel.spencer@physics.ox.ac.uk>
2021-01-05 11:52:11 +0000

    New dataloader

--- dataloader.py
+++ dataloader.py
@@ -2,30 +2,29 @@ import h5py
 import numpy as np
 import glob
 
-filelist = sorted(glob.glob('/path/to/data/*.hdf5'))
+filelist = sorted(glob.glob('/mnt/extraspace/exet4487/pointrun3/*.hdf5'))
+
+filelist=filelist[:5]
 
 def generate_training_data(filelist):
     """ Generates training/test sequences on demand
     """
 
-    nofiles = 0
-    i = 0  # No. events loaded in total
-
+    allcharges=[]
+    alllabels=[]
     for file in filelist:
         inputdata = h5py.File(file, 'r')
-        chargearr = np.asarray(inputdata['squared_training'][:, 0, :, :])
-        labelsarr = np.asarray(inputdata['event_label'][:])
-        valilocs = np.where(labelsarr!=-1)[0]
-        labelsarr = labelsarr[valilocs]
-        chargearr = chargearr[valilocs,:,:]
-        idarr = np.asarray(inputdata['id'][:])
-        nofiles = nofiles + 1
-        inputdata.close()
-        chargearr = np.reshape(chargearr, (np.shape(chargearr)[0], 48, 48, 1)) 
-        training_sample_count = len(chargearr)
-        i = i + len(labelsarr)
-        countarr = np.arange(0, len(labelsarr))
-        ta2 = np.zeros((training_sample_count, 48, 48, 1))   
-        ta2[:, :, :, 0] = chargearr[:, :, :, 0]
-        trainarr = ta2
-    return trainarr, labelsarr
+        print(np.shape(inputdata['event_label']))
+        for j in np.arange(np.shape(inputdata['event_label'])[0]):
+            chargearr = inputdata['squared_training'][j, 0, :, :]
+            labelsarr = inputdata['event_label'][j]
+            allcharges.append(chargearr)
+            alllabels.append(labelsarr)
+    allcharges=np.asarray(allcharges)
+    alllabels=np.asarray(alllabels)
+    return allcharges, alllabels
+
+x,y=generate_training_data(filelist)
+print(np.shape(x),np.shape(y))
+print(x[0])
+print(y)
